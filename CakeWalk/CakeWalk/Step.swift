334bff740556548a4e25be2a4e8a9c7289333367
sahil-sahu-dev <sahilsahucode@gmail.com>
2022-01-01 18:01:03 +0530

    changed the model. Contains user uid, steps count, and obj is identifiable using id{uid}

--- CakeWalk/CakeWalk/Step.swift
+++ CakeWalk/CakeWalk/Step.swift
@@ -8,35 +8,11 @@
 import Foundation
 
 
-struct Step: Identifiable {
-    var id = UUID()
+struct StepUser: Identifiable {
+    
+    var id: String{uid}
+    var uid: String
     let count: Int
-    
-}
-
-
-struct StepCounterModel {
-    
-    private var steps: Step?
-
-    public mutating func setSteps(with steps: Step) {
-        self.steps = steps
-    }
-    
-    public func getSteps() -> Step? {
-        return steps
-    }
-    
-    public func getId() -> UUID? {
-        if let step = steps {
-            return step.id
-        }
-        return nil
-    }
-    
-    public func getStepsCount() -> Int? {
-        return steps?.count
-    }
-    
+    let name: String
     
 }
