e2af8bb69402b2348ce425b8ff9319689f9339d0
sahil-sahu-dev <sahilsahucode@gmail.com>
2022-01-13 13:13:27 +0530

    view changes.removed tab view

--- CakeWalk/CakeWalk/AllUsersView.swift
+++ CakeWalk/CakeWalk/AllUsersView.swift
@@ -11,41 +11,66 @@ struct AllUsersView: View {
     
     
     @EnvironmentObject var allUsersViewModel: AllUsersViewModel
+    @State var isFetchingData = false
+    @State var shouldShowLogOutOptions = false
+    
     
     var healthStore: HealthStore?
-    
     init(healthStore: HealthStore) {
         self.healthStore = healthStore
     }
     
-    @State var shouldShowLogOutOptions = false
+    
     
     
     var body: some View {
         NavigationView {
             
-            
-            ScrollView{
-                HStack{
-                    BarChart(title: "Friends", legend: "", barColor: Color.orange, data: allUsersViewModel.chartData)
-                    Text("👟 \(Int(allUsersViewModel.currentUserCount))")
+            VStack{
+                
+                if self.isFetchingData {
+                    ProgressView()
+                }
+                else{
+                    GeometryReader{ geometry in
+                        ScrollView{
+                            
+                            BarChart(title: "Friends", legend: "", barColor: Color.orange, data: allUsersViewModel.chartData, x: geometry.size.width, y: geometry.size.height).padding()
+                            
+                            Text("👟 Your Steps: \(Int(allUsersViewModel.currentUserCount))")
+                                .padding()
+                            if let pos = allUsersViewModel.allUsers.firstPositionMatching(id: FirebaseManager.shared.auth.currentUser!.uid , in : allUsersViewModel.allUsers) {
+                                Text("You are ranked #\(pos)")
+                            }
+                                                        
+                            Divider().padding()
+                            
+                            
+                            VStack{
+                                ForEach(0..<allUsersViewModel.allUsers.count, id: \.self){ index in
+                                    
+                                    
+                                    VStack{
+                                        
+                                        Text("#\(index+1). \(allUsersViewModel.allUsers[index].name)").padding()
+                                        Divider()
+                                        Text("👟 \(Int(allUsersViewModel.allUsers[index].count ?? 0))").padding()
+                                    }.background(RoundedRectangle(cornerRadius: 25).foregroundColor(.white).shadow(radius: 3))
+                                        .frame(maxWidth: geometry.size.width * 0.6, maxHeight: geometry.size.height * 0.3)
+                                }
+                            }
+                            .padding()
+                            .frame(width: geometry.size.width)
+                            
+                        }
+                    }
                 }
                 
-            
-                ForEach(allUsersViewModel.allUsers){ user in
-                    VStack{
-                        Text(user.name).padding()
-                        Text(String(user.count ?? 0)).padding()
-                    }.background(RoundedRectangle(cornerRadius: 25).foregroundColor(.white).shadow(radius: 3))
-                    
-                    
-                }
             }
-            .background(Color(.init(white: 0, alpha: 0.05))
-                             .ignoresSafeArea())
             
-           
-            .navigationBarTitle("Home")
+            .background(Color(.init(white: 0, alpha: 0.05)).edgesIgnoringSafeArea(.all))
+            
+            .navigationBarTitle("CakeWalk").font(.headline)
             
             .toolbar {
                 ToolbarItem {
@@ -62,6 +87,7 @@ struct AllUsersView: View {
             
             
         }
+        
         .onAppear {
             initializeViews()
         }
@@ -69,29 +95,28 @@ struct AllUsersView: View {
         .actionSheet(isPresented: $shouldShowLogOutOptions) {
             .init(title: Text("Settings"), message: Text("What do you want to do?"), buttons: [
                 .destructive(Text("Sign Out"), action: {
-
+                    
                     allUsersViewModel.handleSignOut()
-
+                    
                     print("handle sign out")
                 }),
                 .cancel()
             ])
         }
-
+        
         .fullScreenCover(isPresented: $allUsersViewModel.isUserCurrentlyLoggedOut, onDismiss: nil) {
             LoginView {
-
+                
                 self.initializeViews()
                 self.allUsersViewModel.isUserCurrentlyLoggedOut = false
-
+                
             }
         }
-        
-        .navigationViewStyle(StackNavigationViewStyle())
     }
     
     
     func initializeViews() {
+        
         
         if let healthStore = healthStore {
             healthStore.requestAuthorization{ success in
@@ -100,11 +125,16 @@ struct AllUsersView: View {
                     healthStore.calculateSteps { statistics in
                         
                         DispatchQueue.main.sync {
+                            
+                            self.isFetchingData.toggle()
+                            
                             allUsersViewModel.updateSteps(with: statistics)
+                            allUsersViewModel.fetchAllUsers()
                             
-                            allUsersViewModel.fetchAllUsers() 
-                            
+                            self.isFetchingData.toggle()
                         }
+                        
+                        
                     }
                     
                 }
